582e7ba065318519375fa88a73e52f97ca18ed81
Mumeeta bai <mumeeta44@gmail.com>
2021-11-16 13:51:17 +0100

    updated

--- src/App.js
+++ src/App.js
@@ -1,25 +1,107 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import Circle from "./Cricle";
+import GameOver from "./GameOver";
+import { circles } from "./cricles";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const getRndInteger = (min, max) => {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+};
+
+class App extends Component {
+  state = {
+    score: 0,
+    current: 0,
+    gameOver: false,
+    pace: 1500,
+  };
+
+  timer = undefined;
+
+  clickHandler = (id) => {
+    console.log("you clicked: ", id);
+
+    if (this.state.current !== id) {
+      this.stopHandler();
+      return;
+    }
+
+    this.setState({
+      score: this.state.score + 10,
+      round: 0,
+    });
+  };
+
+  nextCircle = () => {
+    if (this.state.rounds >= 5) {
+      this.stopHandler();
+      return;
+    }
+    let nextActive;
+
+    do {
+      nextActive = getRndInteger(1, 4);
+    } while (nextActive === this.state.current);
+
+    this.setState({
+      current: nextActive,
+      pace: this.state.pace * 0.95,
+      rounds: this.state.rounds + 1,
+    });
+
+    this.timer = setTimeout(this.nextCircle, this.state.pace);
+
+    console.log("active circle is ", this.state.current);
+  };
+
+  startHandler = () => {
+    this.nextCircle();
+  };
+
+  stopHandler = () => {
+    clearTimeout(this.timer);
+
+    this.setState({
+      gameOver: true,
+      current: 0,
+    });
+  };
+
+  closeHandler = () => {
+    this.setState({
+      gameOver: false,
+      score: 0,
+      pace: 1500,
+    });
+  };
+
+  render() {
+    return (
+      <div>
+        {" "}
+        {this.state.gameOver && (
+          <GameOver score={this.state.score} close={this.closeHandler} />
+        )}{" "}
+        <h1> SpeedGame </h1> <p> Your score: {this.state.score} </p>{" "}
+        <div className="circles">
+          {" "}
+          {circles.map((c) => (
+            <Circle
+              key={c.id}
+              color={c.color}
+              id={c.id}
+              click={() => this.clickHandler(c.id)}
+              active={this.state.current === c.id}
+            />
+          ))}{" "}
+        </div>{" "}
+        <div>
+          <button onClick={this.startHandler}> Start </button>{" "}
+          <button onClick={this.stopHandler}> Stop </button>{" "}
+        </div>{" "}
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/cricles.js
@@ -0,0 +1,6 @@
+export const circles = [
+  { id: 1, color: "#D06224" },
+  { id: 2, color: "8A8635" },
+  { id: 3, color: "#E9C891" },
+  { id: 4, color: "#A2CDCD" },
+];
